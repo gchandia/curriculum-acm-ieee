2e3b86e703dcd04aea88ff5a1350eb14c762bbeb
Emilio Aburto L <emilio-aburto@users.noreply.github.com>
2017-10-05 21:49:37 -0300

    Reordering due to python problems and rename of User class to Teacher, now using an 1-to-1 relation with a Django native User class for better integration with authentication system.

--- cliente/models.py
+++ cliente/models.py
@@ -1,13 +1,6 @@
-from django.utils.translation import ugettext as _
 from django.db import models
-
-
-class User(models.Model):
-    email = models.EmailField(_("Email"))
-    phone = models.CharField(_("Teléfono"), max_length=100)
-    fullname = models.CharField(_("Nombre completo"), max_length=100)
-    institutions = models.ManyToManyField(Institution, verbose_name=(_("Instituciones")))
-    programs = models.ManyToManyField(Program, verbose_name=(_("Carreras")))
+from django.utils.translation import ugettext as _
+from django.contrib.auth.models import User
 
 
 class Institution(models.Model):
@@ -17,26 +10,6 @@ class Institution(models.Model):
 class Campus(models.Model):
     name = models.CharField(_("Nombre del Campus"), max_length=100)
     institution = models.ForeignKey(Institution)
-
-
-class Program(models.Model):
-    name = models.CharField(_("Nombre de la Carrera"), max_length=100)
-    campus = models.ForeignKey(Campus)
-    subjects = models.ManyToManyField(Subject, verbose_name=(_("Cursos obligatorios")))
-    electives = models.ManyToManyField(Subject, verbose_name=(_("Cursos electivos")))
-    core_curriculum = models.BooleanField(verbose_name=(_("Plan común")))
-    graduation_time = models.PositiveIntegerField(_("Tiempo estimado de graduación"))
-    # Alumni quantity stats
-    currentyear_alumni_qty = models.PositiveIntegerField(_("Cantidad de alumnos año actual"))
-    lastyear_alumni_qty = models.PositiveIntegerField(_("Cantidad de alumnos año pasado"))
-    b4lastyear_alumni_qty = models.PositiveIntegerField(_("Cantidad de alumnos año antepasado"))
-    # Application stats
-    currentyear_psu_max = models.PositiveIntegerField(_("Máximo puntaje PSU año actual"))
-    currentyear_psu_min = models.PositiveIntegerField(_("Mínimo puntaje PSU año actual"))
-    lastyear_psu_max = models.PositiveIntegerField(_("Máximo puntaje PSU año pasado"))
-    lastyear_psu_min = models.PositiveIntegerField(_("Mínimo puntaje PSU año pasado"))
-    b4lastyear_psu_max = models.PositiveIntegerField(_("Máximo puntaje PSU año antepasado"))
-    b4lastyear_psu_min = models.PositiveIntegerField(_("Mínimo puntaje PSU año antepasado"))
 
 
 class Subject(models.Model):
@@ -63,3 +36,37 @@ class Subject(models.Model):
     aux_amount = models.PositiveIntegerField(_("Número de auxiliares"))
     ayu_amount = models.PositiveIntegerField(_("Número de ayudantes"))
     tut_amount = models.PositiveIntegerField(_("Número de tutores"))
+
+
+class Program(models.Model):
+    name = models.CharField(_("Nombre de la Carrera"), max_length=100)
+    campus = models.ForeignKey(Campus)
+    subjects = models.ManyToManyField(Subject,
+                                      verbose_name=_("Cursos obligatorios"),
+                                      related_name="programs")
+    electives = models.ManyToManyField(Subject,
+                                       verbose_name=_("Cursos electivos"),
+                                       related_name="elective_programs")
+    core_curriculum = models.BooleanField(verbose_name=_("Plan común"))
+    graduation_time = models.PositiveIntegerField(_("Tiempo estimado de graduación"))
+    # Alumni quantity stats
+    currentyear_alumni_qty = models.PositiveIntegerField(_("Cantidad de alumnos año actual"))
+    lastyear_alumni_qty = models.PositiveIntegerField(_("Cantidad de alumnos año pasado"))
+    b4lastyear_alumni_qty = models.PositiveIntegerField(_("Cantidad de alumnos año antepasado"))
+    # Application stats
+    currentyear_psu_max = models.PositiveIntegerField(_("Máximo puntaje PSU año actual"))
+    currentyear_psu_min = models.PositiveIntegerField(_("Mínimo puntaje PSU año actual"))
+    lastyear_psu_max = models.PositiveIntegerField(_("Máximo puntaje PSU año pasado"))
+    lastyear_psu_min = models.PositiveIntegerField(_("Mínimo puntaje PSU año pasado"))
+    b4lastyear_psu_max = models.PositiveIntegerField(_("Máximo puntaje PSU año antepasado"))
+    b4lastyear_psu_min = models.PositiveIntegerField(_("Mínimo puntaje PSU año antepasado"))
+
+
+class Teacher(models.Model):
+    # User model includes username, email, first_name, last_name, password and other fields
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # email = models.EmailField(_("Email"))
+    # fullname = models.CharField(_("Nombre completo"), max_length=100)
+    phone = models.CharField(_("Teléfono"), max_length=100)
+    institutions = models.ManyToManyField(Institution, verbose_name=_("Instituciones"))
+    programs = models.ManyToManyField(Program, verbose_name=_("Carreras"))
